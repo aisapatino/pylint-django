8a2eb456b45e270446ca4372b52aab5a228b2782
Anna Luisa Patiño West <anna.luisa.west@formidablelabs.com>
2016-08-21 20:05:39 -0700

    hack to avoid no-member errors for test responses

--- pylint_django/transforms/transforms/django_core_handlers_wsgi.py
+++ pylint_django/transforms/transforms/django_core_handlers_wsgi.py
@@ -4,3 +4,11 @@ from django.core.handlers.wsgi import WSGIRequest as WSGIRequestOriginal
 class WSGIRequest(WSGIRequestOriginal):
     status_code = None
     content = ''
+
+    # hacks for tests, where astroid always parses response as WSGIRequest
+    # even though it may actually be a different class (HttpResponseRedirect, etc)
+    context = {}
+    url = ''
+
+    def get(header):
+        pass
